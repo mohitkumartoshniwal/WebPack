f7aacaf531c639e3ecb7cb65d7015813368381f6
mohitkumartoshniwal <mohitkrtoshniwal.com>
2020-04-17 01:34:17 +0530

    configured babebl for transpiling code to es5 and added loaders for styling

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
 import component from "./component";
+import './style.css'
+
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,25 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+          },
+          
+        ]
+      },
     plugins: [
         new HtmlWebpackPlugin({
             title: "Webpack Output",
